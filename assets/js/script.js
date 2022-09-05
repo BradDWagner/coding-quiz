9f519133f3a8170e4b98ddd826215a90ffd82eb8
BradDWagner <110359842+BradDWagner@users.noreply.github.com>
2022-09-04 21:53:19 -0700

    created JS function to create end screen and started setting up event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,20 @@ var card = document.querySelector(".card");
 
 
 
+
+
+var questionArray = [
+    ["Comonly used data types DO NOT include:", "1. Strings", "false", "2. Booleans", "false", "3. Alerts", "true", "4. Numbers", "false"],
+    ["The condition in and if/else statement is enclosed withing ______:", "1. Quotes", "false", "2. Curly brackets", "false", "3. Parentheses", "true", "4. Square brackets", "false"]
+]
+
+var lastAnswerState = "";
+
+
+
 function generateStart () {
+    card.innerHTML = "";
+
     var startDiv = document.createElement("div");
     startDiv.setAttribute("class", "start-screen");
     card.appendChild(startDiv);
@@ -23,12 +36,15 @@ function generateStart () {
 }
 // generateStart(); 
 
-var questionArray = [
-    ["Comonly used data types DO NOT include:", "1. Strings", "false", "2. Booleans", "false", "3. Alerts", "true", "4. Numbers", "false"],
-    ["The condition in and if/else statement is enclosed withing ______:", "1. Quotes", "false", "2. Curly brackets", "false", "3. Parentheses", "true", "4. Square brackets", "false"]
-]
+
+
+//function to cycle through quiz screens
+    //function to create/append elements
+    //function to set attributes
 
 function generateQuiz (questionNumber){
+    card.innerHTML = "";
+
     var quizDiv= document.createElement("div");
     quizDiv.setAttribute("class","quiz-screen");
     card.appendChild(quizDiv);
@@ -38,13 +54,13 @@ function generateQuiz (questionNumber){
     quizDiv.appendChild(quizHeader);
 
     var quizButtonDiv =document.createElement("div");
+    quizButtonDiv.setAttribute("id", "quiz-screen-buttons")
     quizDiv.appendChild(quizButtonDiv);
 
-    for (x=1; x < 9; x = x +2){
+    for (var x=1; x < 9; x = x +2){
         var counter = ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10"]
         counter[x] = document.createElement("button");
         counter[x].textContent = questionArray[questionNumber][x];
-      
         counter[x].setAttribute("data-boolean", questionArray[questionNumber][x+1]);
         quizButtonDiv.appendChild(counter[x]);
     }
@@ -54,11 +70,103 @@ function generateQuiz (questionNumber){
     quizDiv.appendChild(quizLastAnswer);
  
 }
-generateQuiz(1);
+// generateQuiz(1);
 
-//function to cycle through quiz screens
-    //function to create/append elements
-    //function to set attributes
+
+
 
 //function to generate end screen
+function generateEnd () {
+    var endDiv = document.createElement("div");
+    endDiv.setAttribute("class", "end-screen");
+    card.appendChild(endDiv);
 
+    var endHeader = document.createElement("h1");
+    endHeader.textContent = "All done!";
+    endDiv.appendChild(endHeader);
+
+    var endP = document.createElement("p");
+    endP.setAttribute("id", "display-score");
+    endDiv.appendChild(endP);
+
+    var endForm = document.createElement("form");
+    endDiv.appendChild(endForm);
+
+    var endFormLabel = document.createElement("label");
+    endFormLabel.setAttribute("class", "input-label");
+    endFormLabel.setAttribute("for", "initial-form");
+    endFormLabel.textContent = "Enter initials:";
+    endForm.appendChild(endFormLabel);
+
+    var endFormInput = document.createElement("input");
+    endFormInput.setAttribute("type", "text");
+    endFormInput.setAttribute("id", "initial-form");
+    endForm.appendChild(endFormInput);
+
+    var endFormButton = document.createElement("button");
+    endFormButton.setAttribute("type", "submit");
+    endFormButton.textContent = "Submit";
+    endForm.appendChild(endFormButton);
+
+    var endLastAnswer = document.createElement("p");
+    endLastAnswer.setAttribute("class", "last-answer");
+    endDiv.appendChild(endLastAnswer);
+}
+// generateEnd();
+
+
+// var quizScreenButtons = document.querySelector("#quiz-screen-buttons");
+
+function startQuiz () {
+    generateStart();
+    var startScreenButton = document.querySelector("#start-quiz");
+
+    startScreenButton.addEventListener("click", function(){
+
+
+        // i = 0;
+        // generateQuiz(i);
+        // var quizScreenButtons = document.querySelector("#quiz-screen-buttons");
+        // quizScreenButtons.addEventListener("click", function(event){
+        //     var element = event.target;
+        //     if (element.matches("button")){
+        //         var state = element.getAttribute("data-boolean");
+        //         if (state == "true") {
+        //             lastAnswerState = "true";
+        //             i++;
+        //             generateQuiz(i);
+        //         } else {
+        //             lastAnswerState = "false";
+                    
+        //         }
+        //        console.log(lastAnswerState);
+        //     }
+             
+        // })
+
+
+        for (var i = 0; i < questionArray.length; i = i+0) {
+            generateQuiz(i);
+            // var quizScreenButtons = document.querySelector("#quiz-screen-buttons");
+
+            quizScreenButtons.addEventListener("click", function(event) {
+                var element = event.target;
+
+                if (element.matches("button")) {
+                    var state = element.getAttribute("data-boolean");
+
+                    if (state == "true") {
+                        lastAnswerState = "true";
+                        i++;
+                    } else {
+                        lastAnswerState = "false";
+                        i++;
+                    }
+                }
+            })
+        }
+
+
+    })
+}
+startQuiz(); 

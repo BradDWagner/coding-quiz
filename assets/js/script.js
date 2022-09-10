13b42ea4a9a1f714f307a08c86bbc9617f811425
BradDWagner <110359842+BradDWagner@users.noreply.github.com>
2022-09-09 21:57:55 -0700

    created highscore screen and integrated local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var card = document.querySelector(".card");
 
 
 
-
+//Array containing arrays of text data for quiz questions. Each nested array contains prompt at the 0 index, button options at the odd indexes, and true/false data on subsequent even indexes
 var questionArray = [
     ["Comonly used data types DO NOT include:", "1. Strings", "false", "2. Booleans", "false", "3. Alerts", "true", "4. Numbers", "false"],
     ["The condition in and if/else statement is enclosed withing ______:", "1. Quotes", "false", "2. Curly brackets", "false", "3. Parentheses", "true", "4. Square brackets", "false"],
@@ -19,14 +19,16 @@ var timerCount = 75;
 var timerDisplay = document.querySelector(".display-timer");
 timerDisplay.textContent = "Time: 75";
 var finalScore;
+var highScoreArray = [];
 
 
 
-
-
+//function to generate starting screen
 function generateStart () {
     card.innerHTML = "";
+    timerCount = 75;
 
+    //create HTML elements for start screen
     var startDiv = document.createElement("div");
     startDiv.setAttribute("class", "start-screen");
     card.appendChild(startDiv);
@@ -44,6 +46,7 @@ function generateStart () {
     startButton.setAttribute("id", "start-quiz")
     startDiv.appendChild(startButton);
 
+    //add event listener to start button which calls first question and starts timer
     var startScreenButton = document.querySelector("#start-quiz");
     startScreenButton.addEventListener("click", function(){
 
@@ -52,17 +55,25 @@ function generateStart () {
 
     })
 }
-generateStart(); 
 
+//function to start the timer.
+function startTimer(){
+    timer = setInterval(function(){
+        timerCount--;
+        timerDisplay.textContent = "Time: " + timerCount;
+        if (timerCount < 1){
+            clearInterval(timer);
+            timerCount = 0; 
+            generateEnd();
+        }
+    }, 1000);
+}
 
-
-//function to cycle through quiz screens
-    //function to create/append elements
-    //function to set attributes
-
+//function to generate quiz questions. The questionNumber parameter will refer to which array within the questionArray to use
 function generateQuiz (questionNumber){
     card.innerHTML = "";
 
+    //create HTML
     var quizDiv= document.createElement("div");
     quizDiv.setAttribute("class","quiz-screen");
     card.appendChild(quizDiv);
@@ -75,21 +86,24 @@ function generateQuiz (questionNumber){
     quizButtonDiv.setAttribute("id", "quiz-screen-buttons")
     quizDiv.appendChild(quizButtonDiv);
 
+    //use a for loop to create each of the four buttons. cycles through the questionsArray to insert corresponding values
     for (var x=1; x < 9; x = x +2){
-        var counter = ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10"]
-        counter[x] = document.createElement("button");
-        counter[x].textContent = questionArray[questionNumber][x];
-        counter[x].setAttribute("data-boolean", questionArray[questionNumber][x+1]);
-        quizButtonDiv.appendChild(counter[x]);
+        button = document.createElement("button");
+        button.textContent = questionArray[questionNumber][x];
+        button.setAttribute("data-boolean", questionArray[questionNumber][x+1]);
+        quizButtonDiv.appendChild(button);
     }
 
+    //add event listener
     var quizScreenButtons = document.querySelector("#quiz-screen-buttons");
     quizScreenButtons.addEventListener("click", function(event) {
         var element = event.target;
 
+        //refrence the true/false data stored on each button
         if (element.matches("button")) {
             var state = element.getAttribute("data-boolean");
 
+            //if true, sets the lastAnswerState variable to Correct and cycles to the next question if there is one, otherwise clears timer and takes you to the end screen
             if (state == "true") {
                 lastAnswerState = "Correct!";
                 if (questionNumber + 1 < questionArray.length){
@@ -101,10 +115,18 @@ function generateQuiz (questionNumber){
                     generateEnd();
                 }
                 
-                
+            //if answer is incorrect changes lastAnswerState to Wrong. 
             } else {
                 lastAnswerState = "Wrong!";
-                timerCount = timerCount - 20;
+                if (timerCount > 19) {
+                    timerCount = timerCount - 20;
+                } else {
+                    timerCount = 0;
+                    finalScore = timerCount;
+                    clearInterval(timer);
+                    generateEnd();
+                }
+                
                 if (questionNumber + 1 < questionArray.length){
                     generateQuiz(questionNumber+1);
                 } else {
@@ -116,24 +138,22 @@ function generateQuiz (questionNumber){
         }
     })
 
+    //using the lastAnswerState variable displays a bit of text to inform the user if the last answer was right or wrong for 1 second
     var quizLastAnswer = document.createElement("p");
     quizLastAnswer.setAttribute("class", "last-answer");
     quizLastAnswer.textContent = lastAnswerState;
     quizDiv.appendChild(quizLastAnswer);
-    
- 
+
+    setTimeout(function() {
+        quizDiv.removeChild(quizLastAnswer);
+    }, 1000);
 }
-// generateQuiz(4);
-
-
-
-
-
 
 //function to generate end screen
-function generateEnd () {
+function generateEnd() {
     card.innerHTML = "";
 
+    //create HTML elements
     var endDiv = document.createElement("div");
     endDiv.setAttribute("class", "end-screen");
     card.appendChild(endDiv);
@@ -171,107 +191,88 @@ function generateEnd () {
     endLastAnswer.textContent = lastAnswerState;
     endDiv.appendChild(endLastAnswer);
 
+    setTimeout(function() {
+        endDiv.removeChild(endLastAnswer);
+    }, 1000);
+
     var initialInput = document.querySelector("input");
     var initialSubmit = document.querySelector("button");
 
+    //collects input of the users initials from the form element, adds it to the users final score and stores it in the highScoreArray, and takes the user to the highScores screen
     initialSubmit.addEventListener("click", function(event){
         var initialText = initialInput.value.trim();
         if (initialText === "") {
             return;
         }
 
-        var finalScoreArray = [];
-        finalScoreArray.push(initialText);
-        finalScoreArray.push(finalScore); 
+        highScoreArray.push(initialText + " - " + finalScore);
+        console.log(highScoreArray);
 
-        localStorage.setItem("highScores", JSON.stringify(finalScoreArray));
+        generateHighScores();
     })
 
     
 }
-// generateEnd();
 
+//function to generate Highscores screen
+function generateHighScores (){
 
-function startTimer(){
-    timer = setInterval(function(){
-        timerCount--;
-        timerDisplay.textContent = "Time: " + timerCount;
-        if (timerCount === 0){
-            clearInterval(timer);
-            generateEnd();
-        }
-    }, 1000);
+    card.innerHTML = "";
+    //set the highScoreArray to local storage
+    localStorage.setItem("highScores", JSON.stringify(highScoreArray));
+
+    //create HTML elements
+    var HighScoreDiv = document.createElement("div");
+    HighScoreDiv.setAttribute("class", "highscores-screen");
+    card.appendChild(HighScoreDiv);
+
+    var HighScoreHeader = document.createElement("h1");
+    HighScoreHeader.textContent = "High Scores";
+    HighScoreDiv.appendChild(HighScoreHeader);
+
+    var HighScoreOl = document.createElement("ol");
+    HighScoreDiv.appendChild(HighScoreOl);
+
+    //create a li for each set of scores stored in the highScoreArray
+    for (var i = 0; i < highScoreArray.length; i++) {
+      
+        var li = document.createElement("li");
+        li.textContent = highScoreArray[i];
+
+        HighScoreOl.appendChild(li);
+    }
+
+    //create button to play again, and reset starting values
+    var returnButton = document.createElement("button");
+    returnButton.setAttribute("type", "button");
+    returnButton.textContent = "Go Back";
+    HighScoreDiv.appendChild(returnButton);
+    returnButton.addEventListener("click", function(){
+        lastAnswerState = "";
+        timerCount = 75;
+        generateStart();
+    })
+
+    //create button to clear the highScores for the highScoreArray and localStorage and regenerate the screen
+    var clearButton = document.createElement("button");
+    clearButton.setAttribute("type", "button");
+    clearButton.textContent = "Clear Highscores";
+    HighScoreDiv.appendChild(clearButton);
+    clearButton.addEventListener("click", function(){
+        highScoreArray = [];
+        localStorage.clear();
+        generateHighScores();
+    })
+
 }
 
-
-
-// function startQuiz () {
-//     generateStart();
-//     var startScreenButton = document.querySelector("#start-quiz");
-
-//     startScreenButton.addEventListener("click", function(){
-
-//         generateQuiz(0);
-//         startTimer(); 
-
-//     })
-// }
-// // startQuiz(); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // i = 0;
-        // generateQuiz(i);
-        // var quizScreenButtons = document.querySelector("#quiz-screen-buttons");
-        // quizScreenButtons.addEventListener("click", function(event){
-        //     var element = event.target;
-        //     if (element.matches("button")){
-        //         var state = element.getAttribute("data-boolean");
-        //         if (state == "true") {
-        //             lastAnswerState = "true";
-        //             i++;
-        //             generateQuiz(i);
-        //         } else {
-        //             lastAnswerState = "false";
-                    
-        //         }
-        //        console.log(lastAnswerState);
-        //     }
-             
-        // })
-
-
-        // for (var i = 0; i < questionArray.length; i = i+0) {
-        //     generateQuiz(i);
-        //     var quizScreenButtons = document.querySelector("#quiz-screen-buttons");
-
-        //     quizScreenButtons.addEventListener("click", function(event) {
-        //         var element = event.target;
-
-        //         if (element.matches("button")) {
-        //             var state = element.getAttribute("data-boolean");
-
-        //             if (state == "true") {
-        //                 lastAnswerState = "true";
-        //                 i++;
-        //             } else {
-        //                 lastAnswerState = "false";
-        //                 i++;
-        //             }
-        //         }
-        //     })
-        // }
+//function to initialize the page. retrieves highScore data from localStorage and stores it in the highScoreArray, and calls the generateStart function
+function init() {
+    var storedHighScores = JSON.parse(localStorage.getItem("highScores"));
+    if (storedHighScores !== null) {
+        highScoreArray = storedHighScores;
+    }
+    console.log(highScoreArray);
+    generateStart();
+}
+init();
